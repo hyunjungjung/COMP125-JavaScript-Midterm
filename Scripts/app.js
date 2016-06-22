0d71f4cf9d0377aff2b407eede4911d0cb594820
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-06-22 14:03:02 -0400

    Final commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -39,6 +39,7 @@
     paragraphs[8] = "<Br>Most famous for being the home of Odysseus, and a prominent figure in Homer's 'The Odyssey, ' Ithaca is said to have been inhabited since the 2nd millennium BC. Made up of two islands joined by a narrow strip of land, many visit it to see the legendary sites mentioned by Homer. However, Ithaca is more than its mythical counterpart, beckoning with secluded beaches, dramatic cliffs, beautiful olive groves, and sleepy fishing villages full of Byzantine churches and monasteries."
     paragraphs[9] = "<br>Near the more well-known Crete, Gavdos is the most southern island in Greece — and the most southern spot in Europe discounting the Canaries. Only accessible by ferry, the remote island has only about 50 permanent residents, and can feel like your private playground. Local legend has it that the island was the home of goddess Calypso, who kept Odysseus prisoner here. Today, a favorite tourist activity is to visit the spot believed to be her cave. Be warned, you won't find any luxury hotels here. While the romantically under-developed, super laid back island has a number of rooms for rent, those are limited, as the real draw here for many is the free, seaside camping."
 
+    // check to see if paragraph one exists
     var paragraphElementsLength = paragraphElements.length;
     for (var index = paragraphElementsLength; index >= 0; index--) {
         if (paragraphElements[index]) {
@@ -50,7 +51,7 @@
     var lastName = document.getElementById("lastName");
     var email = document.getElementById("email");
     var contactNumber = document.getElementById("contactNumber");
-    var message = document.getElementById("message");
+    var yourMessage = document.getElementById("yourMessage");
 
     // reference to the form
     var contactForm = document.getElementById("contactForm");
